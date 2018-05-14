f74b61f1acfd8ac548413465cdb80b6638410c84
orco <orco@paranoici.org>
2018-05-14 21:46:29 +0200

    Update sleuth.R

--- scripts/sleuth.R
+++ scripts/sleuth.R
@@ -1,4 +1,7 @@
-suppressMessages({library("sleuth")})
+suppressMessages({
+	library("sleuth")
+	library(biomaRt)
+})
 sample_id <- t(read.delim(file.path(snakemake@params["sample_tsv"]), header = TRUE, sep="\t")["sample"])
 kal_dirs <- file.path(snakemake@params["kal_dirs"], sample_id)
 print("Directories containing kallisto results: \n")
@@ -25,3 +28,32 @@ print("Saving plot")
 ggplot2::ggsave(filename="data/output/plot.pdf", plot=sleuth_plot)
 print("Writing results to output")
 write(t(sleuth_significant), file = file.path(snakemake@params["outfile"]), sep = "\t")
+
+## Including gene names into transcript-level analysis
+
+print("Downloading from ensembl.org")
+#source("http://bioconductor.org/biocLite.R")
+#biocLite("biomaRt")
+
+mart <- biomaRt::useMart(biomart = "ENSEMBL_MART_ENSEMBL",
+  dataset = "scerevisiae_gene_ensembl",
+  host = 'host = "ensembl.org"')
+print("2")
+t2g <- biomaRt::getBM(attributes = c("ensembl_transcript_id", "ensembl_gene_id",
+    "external_gene_name"), mart = mart)
+print("2.2")
+t2g <- dplyr::rename(t2g, target_id = ensembl_transcript_id,
+  ens_gene = ensembl_gene_id, ext_gene = external_gene_name)
+print("3")
+so <- sleuth_prep(s2c, target_mapping = t2g)
+so <- sleuth_fit(so, ~condition, 'full')
+so <- sleuth_fit(so, ~1, 'reduced')
+so <- sleuth_lrt(so, 'reduced', 'full')
+print("4")
+pca_plot <- plot_pca(so, color_by = 'condition')
+print("Printing PCA plot")
+ggplot2::ggsave(filename="data/output/plot_pca.pdf", plot=pca_plot)
+plot_group_density(so, use_filtered = TRUE, units = "est_counts",
+  trans = "log", grouping = setdiff(colnames(so$sample_to_covariates),
+  "sample"), offset = 1)
+
